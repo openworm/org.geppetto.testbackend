37da272e11a1a08f4bade9499d0d9160b3b49eae
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-06-18 14:55:32 +0100

    Tests update

--- src/main/java/org/geppetto/testbackend/services/DummyModelInterpreterService.java
+++ src/main/java/org/geppetto/testbackend/services/DummyModelInterpreterService.java
@@ -27,154 +27,128 @@ import org.springframework.stereotype.Service;
  * Creates Scene adding randomly positioned particles and geometries
  * 
  * @author jrmartin
- *
+ * 
  */
 @Service
-public class DummyModelInterpreterService implements IModelInterpreter{
+public class DummyModelInterpreterService implements IModelInterpreter
+{
 
 	private static Log logger = LogFactory.getLog(DummyModelInterpreterService.class);
 
-	private final static String TEST_NUMBER = "Test Number";
+	private static final String TEST = "TEST";
 
-	private final static String TEST_ONE = "TEST_ONE";
-
-	private final static String TEST_TWO = "TEST_TWO";
-
-	private final static String TEST_THREE = "TEST_THREE";
-
-	private final static String TEST_FOUR = "TEST_FOUR";
-
-	private final static String TEST_FIVE = "TEST_FIVE";
-
-	private final static String TEST_SIX = "TEST_SIX";
+	private enum TEST_NO
+	{
+		TEST_ONE, TEST_TWO, TEST_THREE, TEST_FOUR, TEST_FIVE, TEST_SIX
+	}
 
 	private Random randomGenerator;
 
-	private static final int FIFTY_GEOMETRIES = 50;
-
-	private static final int ONE_HUNDRED_GEOMETRIES = 100;
-
-	private static final int ONE_THOUSANDTH_GEOMETRIES = 1000;
-
-	public IModel readModel(URL url) throws ModelInterpreterException {
+	public IModel readModel(URL url) throws ModelInterpreterException
+	{
 
 		logger.info("Reading Model using Dummy Motel Interpreter Service");
 
 		ModelWrapper wrapper = new ModelWrapper(UUID.randomUUID().toString());
 
-		String testName = getTestName(url.toString());
+		TEST_NO test = getTestName(url.toString());
 
-		logger.warn("Wrap Model " + testName);
+		logger.warn("Wrap Model " + test);
 
-		wrapper.wrapModel(TEST_NUMBER, testName);
+		wrapper.wrapModel(TEST, test);
 
 		return wrapper;
 	}
 
 	/**
-	 * Number of test to perform is included in the url value. 
+	 * Number of test to perform is included in the url value.
 	 * 
-	 * @param url - Dummy URL used to fullfill format requirements for file.
-	 *              Number of test included in URL at the end  as in ;
-	 *              https://dummy.url/TEST_ONE
+	 * @param url
+	 *            - Dummy URL used to fullfill format requirements for file. Number of test included in URL at the end as in ; https://dummy.url/TEST_ONE
 	 * @return
 	 */
-	private String getTestName(String url){
-
-		if(url.contains(TEST_ONE)){
-			return TEST_ONE;
-		}
-
-		else if(url.contains(TEST_TWO)){
-			return TEST_TWO;
-		}
-
-		else if(url.contains(TEST_THREE)){
-			return TEST_THREE;
-		}
-
-		else if(url.contains(TEST_FOUR)){
-			return TEST_FOUR;
-		}
-
-		else if(url.contains(TEST_FIVE)){
-			return TEST_FIVE;
-		}
-
-		else if(url.contains(TEST_SIX)){
-			return TEST_SIX;
-		}
-		return "";
+	private TEST_NO getTestName(String url)
+	{
+		return TEST_NO.valueOf(url.substring(url.lastIndexOf("/")+1));
 	}
 
 	/**
-	 * Return a newly created scene after adding geometries with
-	 * random positions. 
+	 * Return a newly created scene after adding geometries with random positions.
 	 */
-	public Scene getSceneFromModel(IModel model, StateTreeRoot treeRoot)
-			throws ModelInterpreterException {
+	public Scene getSceneFromModel(IModel model, StateTreeRoot treeRoot) throws ModelInterpreterException
+	{
 
 		logger.info("Using DummyModelInterpreter to create Scene from IModel");
 
-		ModelWrapper modelWrapper = (ModelWrapper)model;
-
-		//Test Number stored inside model wrapper, retrieve it
-		String testNumber = modelWrapper.getModel(TEST_NUMBER).toString();
-
-		//Returning a dummy created scene 
-		return getSceneForTest(testNumber);		
+		ModelWrapper modelWrapper = (ModelWrapper) model;
+		
+		// Returning a dummy created scene
+		return getSceneForTest((TEST_NO)modelWrapper.getModel(TEST));
 	}
 
 	/**
-	 * Creates a Scene with random geometries added. 
-	 * A different scene is created for each different test
+	 * Creates a Scene with random geometries added. A different scene is created for each different test
 	 * 
-	 * @param testNumber - Test Number to be perform
+	 * @param testNumber
+	 *            - Test Number to be perform
 	 * @return
 	 */
-	private Scene getSceneForTest(String testNumber){
+	private Scene getSceneForTest(TEST_NO test)
+	{
 
 		Scene scene = new Scene();
-		List<Entity> sceneEntities; 
-
-		if(testNumber.equals(TEST_ONE)){		
-			sceneEntities = createTestOneEntities(scene);
-			scene.setEntities(sceneEntities);
+		switch (test)
+		{
+			case TEST_ONE:
+				scene.setEntities(createTestOneEntities(scene,100));
+				break;
+			case TEST_TWO:
+				scene.setEntities(createTestOneEntities(scene,10000));
+				break;
+			case TEST_THREE:
+				scene.setEntities(createTestOneEntities(scene,100000));
+				break;
+			case TEST_FOUR:
+				scene.setEntities(createTestTwoEntities(scene,50));
+				break;
+			case TEST_FIVE:
+				scene.setEntities(createTestTwoEntities(scene,500));
+				break;
+			case TEST_SIX:
+				scene.setEntities(createTestTwoEntities(scene,20000));
+				break;
 		}
-
-		else if(testNumber.equals(TEST_TWO)){
-			sceneEntities = createTestTwoEntities(scene);
-			scene.setEntities(sceneEntities);
-		}
-
 		return scene;
 	}
 
 	/**
-	 * Add 100 particles to scene for test 1. 
+	 * Add 100 particles to scene for test 1.
 	 * 
 	 * @param scene
 	 * @return
 	 */
-	private List<Entity> createTestOneEntities(Scene scene){
+	private List<Entity> createTestOneEntities(Scene scene, int numberOfParticles)
+	{
 		List<Entity> sceneEntities = new ArrayList<Entity>();
+		Entity newEntity = new Entity();
+		newEntity.setId("E1");
+		sceneEntities.add(newEntity);
 
-		sceneEntities.add(0, new Entity());
-
-		for(int i =0; i<ONE_HUNDRED_GEOMETRIES ; i++){					
-			//Create a Position
+		for(int i = 0; i < numberOfParticles; i++)
+		{
+			// Create a Position
 			Point position = new Point();
-			position.setX(0.0);
-			position.setY(0.0);
-			position.setZ((double) i);
+			position.setX(getRandomGenerator().nextDouble() * 10);
+			position.setY(getRandomGenerator().nextDouble() * 10);
+			position.setZ(getRandomGenerator().nextDouble() * 10);
 
-			//Create particle and set position
+			// Create particle and set position
 			Particle particle = new Particle();
 			particle.setPosition(position);
-			particle.setId("P" + String.valueOf(i));
+			particle.setId("P" + i);
 
-			//Add created geometries to entities
-			sceneEntities.get(0).getGeometries().add(particle);
+			// Add created geometries to entities
+			newEntity.getGeometries().add(particle);
 		}
 
 		return sceneEntities;
@@ -186,52 +160,58 @@ public class DummyModelInterpreterService implements IModelInterpreter{
 	 * @param scene
 	 * @return
 	 */
-	private List<Entity> createTestTwoEntities(Scene scene){
+	private List<Entity> createTestTwoEntities(Scene scene, int numberOfGeometries )
+	{
 		List<Entity> sceneEntities = new ArrayList<Entity>();
 
-		for(int i =0; i<FIFTY_GEOMETRIES; i++){
+		for(int i = 0; i < numberOfGeometries; i++)
+		{
 
-			//Create a Random position
+			// Create a Random position
 			Point position = new Point();
 			position.setX(0.0);
 			position.setY(0.0);
 			position.setZ(((double) i) + 0.3);
 
-			//Create a new Cylinder
-			Cylinder cylynder = new Cylinder();
-			cylynder.setPosition(position);
-			cylynder.setId("C" + String.valueOf(i));
-			cylynder.setHeight(getRandomGenerator().nextDouble());
-
-			//Create a Random position
+			// Create a Random position
 			Point position2 = new Point();
 			position2.setX(0.0);
 			position2.setY(0.0);
-			position2.setZ(((double) i) + 0.5);
+			position2.setZ(getRandomGenerator().nextDouble() * 100);
 
-			//Create new sphere and set values
+			// Create a new Cylinder
+			Cylinder cylynder = new Cylinder();
+			cylynder.setPosition(position);
+			cylynder.setDistal(position2);
+			cylynder.setId("C" + i);
+			cylynder.setRadiusBottom(getRandomGenerator().nextDouble() * 10);
+			cylynder.setRadiusTop(getRandomGenerator().nextDouble() * 10);
+
+			// Create new sphere and set values
 			Sphere sphere = new Sphere();
 			sphere.setPosition(position2);
-			sphere.setId("S" + String.valueOf(i));
-			sphere.setRadius(3.0);
+			sphere.setId("S" + i);
+			sphere.setRadius(getRandomGenerator().nextDouble() * 10);
 
-			//Entities are null in scene since it's a dummy interpreter
-			//Add new entity before using it
-			sceneEntities.add(i, new Entity());
+			// Add new entity before using it
+			Entity newEntity = new Entity();
+			newEntity.setId("E" + i);
+			sceneEntities.add(newEntity);
 
-			//Add created geometries to entities
-			sceneEntities.get(i).getGeometries().add(cylynder);
-			sceneEntities.get(i).getGeometries().add(sphere);
+			// Add created geometries to entities
+			newEntity.getGeometries().add(cylynder);
+			newEntity.getGeometries().add(sphere);
 		}
 
 		return sceneEntities;
 	}
 
-	private Random getRandomGenerator(){
-		if(randomGenerator == null){
+	private Random getRandomGenerator()
+	{
+		if(randomGenerator == null)
+		{
 			randomGenerator = new Random();
 		}
-
 		return randomGenerator;
 	}
 }
--- src/main/java/org/geppetto/testbackend/services/DummySimulatorService.java
+++ src/main/java/org/geppetto/testbackend/services/DummySimulatorService.java
@@ -5,7 +5,9 @@ import org.apache.commons.logging.LogFactory;
 import org.geppetto.core.common.GeppettoExecutionException;
 import org.geppetto.core.common.GeppettoInitializationException;
 import org.geppetto.core.model.IModel;
+import org.geppetto.core.model.state.SimpleStateNode;
 import org.geppetto.core.model.state.StateTreeRoot;
+import org.geppetto.core.model.values.DoubleValue;
 import org.geppetto.core.simulation.IRunConfiguration;
 import org.geppetto.core.simulation.ISimulatorCallbackListener;
 import org.geppetto.core.simulator.ASimulator;
@@ -28,16 +30,18 @@ public class DummySimulatorService extends ASimulator{
 	@Override
 	public void simulate(IRunConfiguration runConfiguration)
 			throws GeppettoExecutionException {		
+		try {
+			((SimpleStateNode)tree.getChildren().get(0)).addValue(new DoubleValue(0d));
+			getListener().stateTreeUpdated(tree);
+		} catch (GeppettoExecutionException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void initialize(IModel model, ISimulatorCallbackListener listener) throws GeppettoInitializationException
 	{
 		super.initialize(model, listener);
-		try {
-			getListener().stateTreeUpdated(tree);
-		} catch (GeppettoExecutionException e) {
-			e.printStackTrace();
-		}
+		tree.addChild(new SimpleStateNode("dummy"));
 	}
 
 }
