3c8fd833a979710eab6088e4c2ab439c1da8fdec
jrmartin <jesus@metacell.us>
2013-12-19 13:47:29 -0800

    Adding script to dummy test for more testing

---
+++ src/main/resources/TestSimulationScript2.js
@@ -0,0 +1,9 @@
+Simulation.start();
+
+Simulation.stop();
+
+Simulation.load("https://raw.github.com/openworm/org.geppetto.samples/master/SPH/ElasticSmall/GEPPETTO.xml");
+
+Simulation.pause();
+
+Simulation.start();
