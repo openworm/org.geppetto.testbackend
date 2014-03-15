a5896c819d99367ae89848e6276d18456a89905a
jrmartin <jesus@metacell.us>
2014-03-15 12:38:56 -0700

    test

--- src/main/resources/TestSimulationScript.js
+++ src/main/resources/TestSimulationScript.js
@@ -6,7 +6,7 @@ Simulation.startWatch();
 
 G.addWidget(Widgets.PLOT);
 
-var options = {yaxis:{min:0,max:1},xaxis:{min:0,max:100,show:false}};
+var options = {yaxis:{min:0,max:1},xaxis:{min:0,max:40,show:false}};
 
 Plot1.setOptions(options)
 
@@ -15,3 +15,13 @@ Plot1.plotState("dummyDouble");
 Plot1.setPosition(400,300);
 
 Plot1.setSize(300, 400);
+
+G.addWidget(Widgets.PLOT);
+
+Plot2.setOptions(options)
+
+Plot2.plotState("dummyFloat");
+
+G.wait(1000);
+
+Plot1.plotState("dummyFloat");
