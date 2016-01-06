9ef65b6135aa19437b6a106fc7a19ca4a4b8169a
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-01-06 19:01:05 +0000

    getting back takari junit and removing the test junit version

--- src/test/java/org/geppetto/testbackend/test/neuroml/AllGeppettoTests.java
+++ src/test/java/org/geppetto/testbackend/test/neuroml/AllGeppettoTests.java
@@ -1,12 +1,12 @@
-//package org.geppetto.testbackend.test.neuroml;
-//
-//import org.junit.extensions.cpsuite.ClasspathSuite;
-//import org.junit.extensions.cpsuite.ClasspathSuite.ClassnameFilters;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(ClasspathSuite.class)
-//@ClassnameFilters({"!.*AllTests","!.*S3ManagerTest","!.*HDF5ReaderTest"})
-//public class AllGeppettoTests
-//{
-//
-//}
+package org.geppetto.testbackend.test.neuroml;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.ClassnameFilters;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+@ClassnameFilters({"!.*AllTests","!.*S3ManagerTest","!.*HDF5ReaderTest"})
+public class AllGeppettoTests
+{
+
+}
