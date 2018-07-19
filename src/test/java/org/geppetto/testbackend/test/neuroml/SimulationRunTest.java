d39ee6b89a92b248526bfc35a9bd1bf0cd507a40
walrus <walrus@walrus>
2018-07-19 18:57:02 +0200

    persistenceless neuron test

--- src/test/java/org/geppetto/testbackend/test/neuroml/SimulationRunTest.java
+++ src/test/java/org/geppetto/testbackend/test/neuroml/SimulationRunTest.java
@@ -2,14 +2,21 @@
 
 package org.geppetto.testbackend.test.neuroml;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.geppetto.core.common.GeppettoAccessException;
 import org.geppetto.core.common.GeppettoExecutionException;
+import org.geppetto.core.common.GeppettoInitializationException;
 import org.geppetto.core.data.DataManagerHelper;
 import org.geppetto.core.data.DefaultGeppettoDataManager;
 import org.geppetto.core.data.model.ExperimentStatus;
 import org.geppetto.core.data.model.IExperiment;
+import org.geppetto.core.data.model.IGeppettoProject;
+import org.geppetto.core.data.model.IUserGroup;
+import org.geppetto.core.data.model.UserPrivileges;
 import org.geppetto.core.manager.Scope;
 import org.geppetto.core.services.registry.ApplicationListenerBean;
 import org.geppetto.core.simulator.ExternalSimulatorConfig;
@@ -25,30 +32,26 @@ import org.geppetto.simulation.manager.GeppettoManager;
 import org.geppetto.simulator.external.services.NeuronSimulatorService;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SimulationRunTest
 {
-
-	private DBManager db = new DBManager();
-
 	private User user;
-
-	private GeppettoDataManager dataManager;
 	
 	private static GeppettoManager manager = new GeppettoManager(Scope.CONNECTION);
-	
+	private static IGeppettoProject geppettoProject;
+
 
 	public SimulationRunTest()
 	{
-		db.setPersistenceManagerFactory(DBTestData.getPersistenceManagerFactory());
-		dataManager = new GeppettoDataManager();
-		dataManager.setDbManager(db);
 	}
 	
 	/**
@@ -93,33 +96,76 @@ public class SimulationRunTest
 		
 		DataManagerHelper.setDataManager(new DefaultGeppettoDataManager());
 	}
+	
+	/**
+	 * Test method for {@link org.geppetto.simulation.manager.frontend.controllers.GeppettoManager#setUser(org.geppetto.core.data.model.IUser)}.
+	 * 
+	 * @throws GeppettoExecutionException
+	 */
+	@Test
+	public void test01SetUser() throws GeppettoExecutionException
+	{
+		long value = 1000l * 1000 * 1000;
+		List<UserPrivileges> privileges = new ArrayList<UserPrivileges>();
+		privileges.add(UserPrivileges.READ_PROJECT);
+		privileges.add(UserPrivileges.WRITE_PROJECT);
+		privileges.add(UserPrivileges.DOWNLOAD);
+		privileges.add(UserPrivileges.DROPBOX_INTEGRATION);
+		privileges.add(UserPrivileges.RUN_EXPERIMENT);
+		IUserGroup userGroup = DataManagerHelper.getDataManager().newUserGroup("unaccountableAristocrats", privileges, value, value * 2);
+		manager.setUser(DataManagerHelper.getDataManager().newUser("nonna", "passauord", true, userGroup));
+	}
+
+	/**
+	 * Test method for {@link org.geppetto.simulation.manager.frontend.controllers.GeppettoManager#getUser()}.
+	 */
+	@Test
+	public void test02GetUser()
+	{
+		Assert.assertEquals("nonna", manager.getUser().getName());
+		Assert.assertEquals("passauord", manager.getUser().getPassword());
+	}
+	
+//	@Test
+//	public void test03LoadProject() throws IOException, GeppettoInitializationException, GeppettoExecutionException, GeppettoAccessException
+//	{
+//		InputStreamReader inputStreamReader = new InputStreamReader(GeppettoManagerNeuroMLTest.class.getResourceAsStream("/simulationNeuronTest/GEPPETTO.json"));
+//		geppettoProject = DataManagerHelper.getDataManager().getProjectFromJson(TestUtilities.getGson(), inputStreamReader, null);
+//		manager.loadProject("1", geppettoProject);
+//
+//	}
+//
+//	@Test
+//	public void test04ExperimentNeuronRun() throws GeppettoExecutionException, GeppettoAccessException, InterruptedException
+//	{			
+//		List<? extends IExperiment> status = manager.checkExperimentsStatus("1", geppettoProject);
+//		Assert.assertEquals(1, status.size());
+//		Assert.assertEquals(ExperimentStatus.DESIGN, status.get(0).getStatus());
+//		
+//		manager.runExperiment("1", geppettoProject.getExperiments().get(0));
+//		
+//		Thread.sleep(90000);		
+//	}
+	
+	@Test
+	public void test05LoadProject() throws IOException, GeppettoInitializationException, GeppettoExecutionException, GeppettoAccessException
+	{
+		InputStreamReader inputStreamReader = new InputStreamReader(GeppettoManagerNeuroMLTest.class.getResourceAsStream("/simulationJNeuromlTest/GEPPETTO.json"));
+		geppettoProject = DataManagerHelper.getDataManager().getProjectFromJson(TestUtilities.getGson(), inputStreamReader, null);
+		manager.loadProject("1", geppettoProject);
+
+	}
 
 	@Test
-	public void testExperimentRun() throws GeppettoExecutionException, GeppettoAccessException, InterruptedException
-	{
-//		user = db.findUserByLogin("guest1");
-//		
-//		// If I get the project like this experiment doesn't have a parent
-//		//GeppettoProject project = user.getGeppettoProjects().get(0);
-//		
-//		//dataManager.getGeppettoProjectsForUser("guest1");
-//		GeppettoProject project = dataManager.getGeppettoProjectById(1l);
-//		
-//		List<? extends IExperiment> status = manager.checkExperimentsStatus("1", project);
-//		Assert.assertEquals(3, status.size());
-//		Assert.assertEquals(ExperimentStatus.DESIGN, status.get(0).getStatus());
-//		Assert.assertEquals(ExperimentStatus.COMPLETED, status.get(1).getStatus());
-//		
-//		manager.setUser(user);
-//		manager.runExperiment("1", project.getExperiments().get(0));
-//		
-//		Thread.sleep(30000);
-//		
-//		project = dataManager.getGeppettoProjectById(4l);
-//		manager.runExperiment("1", project.getExperiments().get(0));
-//		
-//		Thread.sleep(30000);
+	public void test06ExperimentJNeuromlRun() throws GeppettoExecutionException, GeppettoAccessException, InterruptedException
+	{			
+		List<? extends IExperiment> status = manager.checkExperimentsStatus("1", geppettoProject);
+		Assert.assertEquals(1, status.size());
+		Assert.assertEquals(ExperimentStatus.DESIGN, status.get(0).getStatus());
 		
+		manager.runExperiment("1", geppettoProject.getExperiments().get(0));
+		
+		Thread.sleep(90000);		
 	}
 
 }
